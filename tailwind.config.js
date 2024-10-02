19a5cc66cfe2f519f1e9593dcd5f481b894286be
kenleyli666 <107999510+kenleyli666@users.noreply.github.com>
2024-10-02 10:56:05 +0800

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        geistSans: "var(--font-geist-sans)",
+      }
+    },
   },
   plugins: [],
 }
